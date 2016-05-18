4a7e9a5858dd2e2ed738b6e6742cdd904a15ed28
Your Name <you.example.com>
2016-05-18 10:51:21 -0400

    Submitting PlayersGuide2.29A

---
+++ PlayersGuide2.29A/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayersGuide2._29A
+{
+    //This is the big bucket
+    class Program
+    {   //This is a smaller bucket
+        static void Main(string[] args)
+            //This is the tiniest bucket
+        {
+            Console.WriteLine("I'm Sorry Dave");
+            Console.ReadKey();
+            //COMMENT
+            /* Lots of words*/
+
+        }
+    }
+}
